fa0cfc978ce069a746906bad19d366d1d90a5842
ymotongpoo <ymotongpoo@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2008-12-10 17:39:44 +0000

    setup script for sqlite3 environment

---
+++ ymotongpoo/Yonda/setup.py
@@ -0,0 +1,27 @@
+#-*- encoding: utf-8 -*-
+
+__author__="ymotongpoo <ymotongpoo@gmail.com>"
+__date__ ="$2008/12/10 00:29:41$"
+__version__="$Revision: 0.10"
+__credits__="0x7d8 -- programming training"
+
+import sqlite3
+
+# create database
+conn = sqlite3.connect('bookmark.db')
+cur = conn.cursor()
+
+cur.execute('create table ttag (id int, tag text)')
+cur.execute('create table tbookmark (url text, title text, user int, tag text, primary key(url, tag))')
+conn.commit()
+
+
+taglist = [u'あとで読む', u'あとでよむ', u'後で読む', u'後でよむ', u'あとで',
+           u'あとで見る', u'後で見る', u'後でみる', u'あとでみる', u'あと',
+           u'いつか読む', u'いつか見る', u'later']
+
+for t in enumerate(taglist):
+    cur.execute('insert into ttag values (?, ?)', t)
+conn.commit()
+
+conn.close()
