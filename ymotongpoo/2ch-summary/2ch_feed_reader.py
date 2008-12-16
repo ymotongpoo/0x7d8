29727badd3c273762fccd56cc133c95e715c8781
ymotongpoo <ymotongpoo@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2008-12-16 08:34:10 +0000

    crawler.py still has some trouble around matching site url

--- ymotongpoo/2ch-summary/2ch_feed_reader.py
+++ ymotongpoo/2ch-summary/2ch_feed_reader.py
@@ -1,83 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import feedparser
 import sqlite3
 from genshi.template import TemplateLoader
 import os
-import time
 
 CODING = 'utf-8'
-TEMPLATE_DIR = os.path.dirname(__file__)
-
-urllist = [
-    u'http://news4vip.livedoor.biz/index.rdf', # 【2ch】ニュー速クオリティ
-    u'http://blog.livedoor.jp/dqnplus/index.rdf', # 痛いニュース
-    u'http://urasoku.blog106.fc2.com/?xml', # ハムスター速報 ２ろぐ
-    u'http://alfalfa.livedoor.biz/index.rdf', # アルファモザイク
-    u'http://koerarenaikabe.livedoor.biz/index.rdf', # 超えられない壁
-    u'http://kanasoku.blog82.fc2.com/?xml', # カナ速
-    u'http://waranote.blog76.fc2.com/?xml', # ワラノート
-    u'http://kaisun1192.blog121.fc2.com/?xml', # おはようｗｗｗお前らｗｗｗｗｗｗｗｗ
-    u'http://vipvipblogblog.blog119.fc2.com/?xml', # ベア速
-    u'http://news4wide.livedoor.biz/index.rdf', # VIPワイドガイド
-    u'http://nukohiroba.blog32.fc2.com/?xml', # ヌコニュース
-    u'http://aresoku.blog42.fc2.com/?xml' # ちょっとアレなニュース
-    ]
-
-
-def get_feed(rsslist):
-    feedlist = []
-    for u in rsslist:
-        try:
-            feedlist.append(feedparser.parse(u))
-        except Exception, e:
-            sys.exc_info()[0]
-            sys.exc_info()[1]
-    
-    return feedlist
-
-
-def rss10feed(feed):
-    entries = []
-    e = {}
-    site = dict(title=feed['feed']['title'].encode(CODING),
-                url=feed['feed']['link'].encode(CODING))
-    
-    for e in feed['entries']:
-        updated = time.strftime('%H:%M', time.strptime(e['updated'], '%Y-%m-%dT%H:%M:%S+09:00'))
-        
-        entries.append(dict(title=e['title'].encode(CODING).strip(),
-                            summary=e['summary'].encode(CODING),
-                            url=e['link'].encode(CODING),
-                            updated=updated.encode(CODING),
-                            site=site))
-    return entries
-
-
-mapfeedfunc = {
-    'livedoor': rss10feed,
-    'fc2.com': rss10feed
-    }
-
+BASE_DIR = os.path.dirname(__file__)
+TEMPLATE_FILE = os.path.join(BASE_DIR, 'template.html')
+DATABASE_FILE = os.path.join(BASE_DIR, '2ch-summary.db')
 
 def main():
-    entrylist = []
-    for feed in get_feed(urllist):
-        feed['feed']['title'].encode(CODING)
-
-        portal = rss10feed
-        for k, v in mapfeedfunc.iteritems():
-            try:
-                feed['feed']['link'].encode(CODING).index(k)
-                portal = k
-            except:
-                continue
-
-        entrylist += mapfeedfunc[portal](feed)
-    feed = None
-
-    loader = TemplateLoader([TEMPLATE_DIR])
-    tmpl = loader.load(os.path.join(TEMPLATE_DIR, 'template.html'))
+    conn = sqlite3.connect(DATABASE_FILE)
+    cur = conn.cursor()
+    cur.execute('select a.*, b.url as site_url from tentry as a inner join tsite as b '
+                'on a.site_id = b.site_id order by a.updated desc')
+    entrylist = cur.fetchall()
+    
+    loader = TemplateLoader([BASE_DIR])
+    tmpl = loader.load(TEMPLATE_FILE)
     stream = tmpl.generate(entries=entrylist)
     print stream.render('html')
 
---
+++ ymotongpoo/2ch-summary/crawler.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import feedparser
+import sqlite3
+import os
+import time
+
+CODING = 'utf-8'
+BASE_DIR = os.path.dirname(__file__)
+DATABASE_FILE = os.path.join(BASE_DIR, '2ch-summary.db')
+
+def get_site_list():
+    sitelist = []
+    conn = sqlite3.connect(DATABASE_FILE)
+    cur = conn.cursor()
+    cur.execute('select site_id, url, title from tsite')
+    for s in cur.fetchall():
+        sitelist.append(dict(id=s[0],
+                             url=s[1],
+                             title=s[2]))
+    return sitelist
+
+
+def search_siteid(url, sitelist):
+    for s in sitelist:
+        if s['url'].index(url):
+            return s['id']
+
+
+def get_feed(rsslist):
+    feedlist = []
+    for u in rsslist:
+        try:
+            feedlist.append(feedparser.parse(u))
+        except Exception, e:
+            sys.exc_info()[0]
+            sys.exc_info()[1]
+    
+    return feedlist
+
+
+def rss10feed(feed):
+    entries = []
+    e = {}
+    site = dict(title=feed['feed']['title'].encode(CODING),
+                url=feed['feed']['link'].encode(CODING))
+    
+    for e in feed['entries']:
+        updated = time.strftime('%Y-%m-%d %H:%M:%S+09:00',
+                                time.strptime(e['updated'], '%Y-%m-%dT%H:%M:%S+09:00'))
+        
+        entries.append(dict(title=e['title'].encode(CODING).strip(),
+                            summary=e['summary'].encode(CODING),
+                            url=e['link'].encode(CODING),
+                            updated=updated.encode(CODING),
+                            site=site))
+    return entries
+
+
+mapfeedfunc = {
+    'livedoor': rss10feed,
+    'fc2.com': rss10feed
+    }
+
+
+def main():
+    # get urllist from DB
+    sitelist = get_site_list()
+    urllist = []
+    for s in sitelist:
+        urllist.append(s['url'])
+
+    # fetch entry data from web
+    entrylist = []
+    for feed in get_feed(urllist):
+        feed['feed']['title'].encode(CODING)
+
+        portal = rss10feed
+        for k, v in mapfeedfunc.iteritems():
+            try:
+                feed['feed']['link'].encode(CODING).index(k)
+                portal = k
+            except:
+                continue
+
+        entrylist += mapfeedfunc[portal](feed)
+
+    # insert entry data into DB
+    conn = sqlite3.connect(DATABASE_FILE)
+    cur = conn.cursor()
+    for e in entrylist:
+        t = (e['url'],)
+        cur.execute('select url from tentry where url = ?', t)
+        if len(cur.fetchall()) == 0:
+            site_id = search_siteid(e['site']['url'], sitelist)
+            t = (e['url'], e['title'], e['summary'], e['updated'], site_id)
+            cur.execute('insert into tentry (url, title, summary, updated, site_id) '
+                        'values (?, ?, ?, datetime(?), ?)', t)
+    conn.commit()
+    conn.close()
+    
+
+if __name__ == '__main__':
+    main()
