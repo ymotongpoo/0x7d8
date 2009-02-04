9e35cd9bec6853e338f48430a7a7f06c2707e52e
ymotongpoo <ymotongpoo@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2009-02-04 00:26:39 +0000

    beta version of mixi downloader

--- ymotongpoo/mixi_download/mixi_download.py
+++ ymotongpoo/mixi_download/mixi_download.py
@@ -6,14 +6,20 @@ __credits__="0x7d8 -- programming training"
 
 import urllib
 import re
-from HTMLParser import HTMLParser, HTMLParseError
+import time
+from StringIO import StringIO
+from lxml import etree
 
-MONTH = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'}
+MONTH = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 YEAR = range(2005, 2010)
+view_url_r = re.compile('^view_diary.pl\?id=[0-9]+&owner_id=[0-9]+$')
 
-class MixiOpener(urllib.FancyURLopener):
+ROOT_URL = 'http://mixi.jp/'
+MONTH_PAGE_URL = 'http://mixi.jp/list_diary.pl'
+
+class MixiExtractor(urllib.FancyURLopener):
     def login(self, email, password):
         LOGIN_URL = 'http://mixi.jp/login.pl'
         params = urllib.urlencode({
@@ -33,105 +39,7 @@ class MixiOpener(urllib.FancyURLopener):
         return r.read()
 
 
-class MixiExtractor():
-    class ExtractEntryUrl(HTMLParser):
-        def __init__(self):
-            HTMLParser.__init__(self)
-            self.codec = 'euc-jp'
-            self.view_url_r = re.compile('^view_diary.pl\?id=[0-9]+&owner_id=[0-9]+$')
-
-            self.urls = []
-
-            self.in_diary_title = False
-
-        def handle_starttag(self, tag, attrs):
-            attrs = dict(attrs)
-            if 'div' == tag and 'class' in attrs:
-                if 'listDiaryTitle' == attrs['class']:
-                    self.in_diary_title = True
-
-            if 'a' == tag and 'href' in attrs and self.in_diary_title:
-                if self.view_url_r.match(attrs['href']) != None:
-                    self.urls.append(attrs['href'])
-
-        def handle_endtag(self, tag):
-            if 'div' == tag and self.in_diary_title:
-                self.in_diary_title = False
-
-        def handle_data(self, data):
-            pass
-
-
-    class ExtractEntryBody(HTMLParser):
-        def __init__(self, comment=False):
-            HTMLParser.__init__(self)
-            self.codec = 'euc-jp'
-            self.comment = comment
-
-            self.entry = {}
-
-            self.in_title = False
-            self.in_title_dt = False
-            self.in_title_dd = False
-            self.in_diary_body = False
-
-        def handle_starttag(self, tag, attrs):
-            attrs = dict(attrs)
-            if 'div' == tag and 'class' in attrs:
-                if 'listDiaryTitle' == attrs['class']:
-                    self.in_title = True
-
-            if 'dt' == tag and self.in_title:
-                self.in_title_dt = True
-
-            if 'dd' == tag and self.in_title:
-                self.in_title_dd = True
-
-            if 'div' == tag and 'id' in attrs:
-                if 'diary_body' == attrs['id']:
-                    self.in_diary_body = True
-
-        def handle_endtag(self, tag):
-            if 'div' == tag and self.in_title:
-                self.in_title = False
-                self.entries.append(entry)
-                self.entry = {}
-
-            if 'dt' == tag and self.in_title_dt:
-                self.in_title_dt = False
-
-            if 'dd' == tag and self.in_title_dd:
-                self.in_title_dd = False
-
-        def handle_data(self, data):
-            if self.in_title_dt:
-                self.entry['title'] = data.decode(self.codec)
-
-            if self.in_title_dd:
-                self.entry['date'] = data.decode(self.codec)
-
-            if self.in_diary_body:
-                self.entry['body'] += data.decode(self.codec)
-
-    def __init__(self, username, password):
-        mo = MixiOpener()
-        mo.login(username, password)
-
-    def __extractContents(self, htmlbody, parser):
-        try:
-            parser = parser
-            parser.feed(htmlbody)
-            parser.close()
-            return parser
-        except HTMLParseError, msg:
-            print 'Error Message: %s' % msg
-
     def getMonthEntryUrls(self, year, month):
-        # magic number
-        offset = 310
-        limit = 895
-        MONTH_PAGE_URL = u'http://mixi.jp/list_diary.pl'
-
         urls = []
         i = 1
         while 1:
@@ -139,52 +47,77 @@ class MixiExtractor():
                 'year': year,
                 'month': month,
                 'page': i})
-            print params
+
             r = self.open(MONTH_PAGE_URL, params)
+            data = r.read()
+            tree = etree.parse(StringIO(data), etree.HTMLParser())
+            whole_urls = [u.attrib['href'].strip() for u in \
+                          tree.xpath('//div[@class="listDiaryTitle"]//a')]
 
-            lines = r.readlines()[offset:limit]
-            data = '\n'.join(lines)
-
-            page_urls = self.__extractContents(data, self.ExtractEntryUrl()).urls
+            page_urls = []
+            for u in whole_urls:
+                if view_url_r.match(u):
+                    page_urls.append(u)
+            
             if not len(page_urls):
                 break
             else:
                 urls += page_urls
                 i += 1
+            
 
-        return urls
+        return map(lambda u:ROOT_URL+u, urls)
 
     def getMonthEntryBody(self, entryurls):
+        def extractEntryContents(node):
+            date = ''.join(node.xpath('//div[@class="listDiaryTitle"]//dd/text()'))
+            title = ''.join(node.xpath('//div[@class="listDiaryTitle"]//dt/text()'))
+            body = ''.join(node.xpath('//div[@id="diary_body"]/text()'))
+            
+            return dict(date=date, title=title, body=body)
+            
         entries = []
+        
         for u in entryurls:
+            print u
             r = self.open(u)
-            lines = r.readlines()[offset:limit]
-            data = '\n'.join(lines)
-            page_entry = self.__extractContents(data, self.ExtractEntrBody()).entry
-            entries.append(page_entry)
+            data = r.read()
+            tree = etree.parse(StringIO(data), etree.HTMLParser())
+            entry = [extractEntryContents(e) \
+                     for e in tree.xpath('//div[@class="viewDiaryBox"]')]
+            for e in entry:
+                entries.append(e)
+
+            print '-'*8, 'waiting next entry...'
+            time.sleep(5)
 
         return entries
 
 
 def downloadMixiEntries(username, password):
-    me = MixiExtratcor(username, password)
+    me = MixiExtractor()
+    me.login(username, password)
     for y in YEAR:
         for m, mon in enumerate(MONTH):
             urls = me.getMonthEntryUrls(y, m)
-            entries = getMonthEntryBody(urls)
+            
+            entries = me.getMonthEntryBody(urls)
 
-            filename = str(y) + mon + '.txt'
-            fp = open(filename)
+            filename = str(y) + str(m+1).zfill(2) + '.txt'
+            fp = open(filename, 'w')
 
             for e in entries:
-                fp.write(entries.date)
-                fp.write(entries.title)
-                fp.write(entries.body)
+                fp.write('='*10 + '\n')
+                fp.write(e['date'] + '\n■' +  e['title'] + '\n' + e['body'] + '\n')
+
+            print '-'*8, 'waiting next month...'
+            time.sleep(10)
 
             fp.close()
 
 
+
 if __name__ == '__main__':
-    downloadMixiEntries('hoge', 'piyo')
+    downloadMixiEntries('ymotongpoo@gmail.com', 'cimoon83')
 
 
