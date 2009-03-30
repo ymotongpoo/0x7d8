9960717d5357e490d3bb072ff147b7c1e074b216
ymotongpoo <ymotongpoo@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2009-03-30 15:42:51 +0000

    just added definition of sort functions

--- ymotongpoo/algorithm/sort/mysort.h
+++ ymotongpoo/algorithm/sort/mysort.h
@@ -9,6 +9,17 @@ public:
 	static void bubble_sort(std::vector<int>& _source);
 	static void selection_sort(std::vector<int>& _source);
 	static void insertion_sort(std::vector<int>& _source);
+	static void shell_sort(std::vector<int>& _soruce);
+	static void merge_sort(std::vector<int>& _source);
+	static void heap_sort(std::vector<int>& _source);
+	static void quick_sort(std::vector<int>& _source);
+	static void intro_sort(std::vector<int>& _source);
+	static void share_sort(std::vector<int>& _source);
+	static void in_place_merge_sort(std::vector<int>& _source);
+	static void cocktail_sort(std::vector<int>& _source);
+	static void comb_sort(std::vector<int>& _source);
+	static void gnome_sort(std::vector<int>& _source);
+	static void odd_even_transportation_sort(std::vector<int>& _source);
 };
 
 #endif
