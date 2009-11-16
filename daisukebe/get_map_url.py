e6969109b3bcc6272fbd83a9cf5abfd321d2e322
poleon.kd <poleon.kd@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2009-11-16 05:43:03 +0000

    modified

--- daisukebe/get_map_url.py
+++ daisukebe/get_map_url.py
@@ -13,20 +13,6 @@ X&oi=geocode_result&ct=title&resnum=1&ved=0CAgQ8gEwAA'
 
     return url_list
 
-"""
-def get_store_name_(soup):
-    name = []
-    for ss in [s for s in soup.findAll("a")]:
-        store_ = re.sub('<a\shref=\"z001\.php\?cmpny_cd=[0-9]\
-{4}&amp\;store_cd=[0-9]{6}&amp;rmode=[0-9]\
-&amp;pg=[a-z][0-9]{4}\"\starget=\"_top\">',
-                        '',
-                        ss.__str__())
-        store__ = re.sub('<br\s/>', '', store_)
-        name.append(re.sub('</a>', '', store__))
-    
-    return name
-"""
 def get_store_name(soup):
     name = []
     for ss in [s for s in soup.findAll("a")]:
@@ -34,8 +20,7 @@ def get_store_name(soup):
         if l == 1:
             name.append(ss.contents[0].__str__())
         elif l == 3:
-            name.append(ss.contents[0].__str__() + ss.contents[1].__str__())
-
+            name.append(ss.contents[0].__str__() + ss.contents[2].__str__())
 
     return name
 
@@ -51,7 +36,7 @@ def get_store_address(soup):
     return address
 
 def main(*argv):
-    b = {'key_word': (u'渋谷'.encode('shift-jis'))}
+    b = {'key_word': (u'三宮'.encode('shift-jis'))}
     url = 'http://www.sevenbank.co.jp/www-cgi/a012.php?sub_option=&'\
         + urllib.urlencode(b) + '&area_code=&qzip=&sql_flg=1&\
 fst_store_csol=&store_cnt=1&city_code='
