654bdbc3096e6399e80b7c41364fe4ff4fce9a81
ymotongpoo@gmail.com <ymotongpoo@gmail.com@2f5d4910-a30b-11dd-98aa-2b35b7097d33>
2010-05-18 13:07:30 +0000

    modified for OAuth

--- ymotongpoo/wisesaw-gae/main.py
+++ ymotongpoo/wisesaw-gae/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 #
 # Copyright 2007 Google Inc.
@@ -16,27 +17,110 @@
 #
 
 
-
-
 import wsgiref.handlers
-from wisesaw import ProcessWisesaw
-
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template, util
+from google.appengine.api import urlfetch
 
-username = 'wisesaw'
-password = 'wisesaw2009'
+from HTMLParser import HTMLParser, HTMLParseError
+
+import oauth
+import tokens
+
+import random
+
+WISE_SAW_URL = 'http://www.meigensyu.com/quotations/view/random'
+UPDATE_URL = 'http://api.twitter.com/1/statuses/update.json'
+
+class ExtractWiseSaw(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.codec = 'utf-8'
+
+    self.wise_saw = u''
+    self.author = u''
+    self.url = u''
+
+    self._in_text = False
+    self._in_link = False
+    self._in_authority = False
+    self._data_count = 0
+
+  def handle_starttag(self, tag, attrs):
+    attrs = dict(attrs)
+    if 'div' == tag and 'class' in attrs:
+      if 'text' == attrs['class']:
+        self._in_text = True
+      elif 'link' == attrs['class']:
+        self._in_link = True
+
+    if 'a' == tag and not self._in_link and self._data_count == 1:
+      self._in_authority = True
+
+  def handle_endtag(self, tag):
+    if 'div' == tag and self._in_text:
+      self._in_text = False
+    if 'div' == tag:
+      self._data_count = 0
+
+  def handle_data(self, data):
+    if self._in_text:
+      self.wise_saw += data.decode(self.codec)
+
+    if self._in_link:
+      self.author = data.decode(self.codec)
+      self._data_count += 1
+      self._in_link = False
+
+    if self._in_authority:
+      self.author += data.decode(self.codec) + ' '
+      self._in_authority = False
+
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    ws = ProcessWisesaw()
-    ws.process(username, password)
-    self.response.out.write('wisesaw')
+    #url = WISE_SAW_URL % random.randint(min_idx, max_idx)
+    url = WISE_SAW_URL
+    result = urlfetch.fetch(url)
+    
+    data = result.content
+
+    # extract text and author from HTML
+    parser = ExtractWiseSaw()
+    parser.feed(data)
+    parser.close()
+    wise_saw = parser.wise_saw.encode(parser.codec)
+    author = parser.author.encode(parser.codec)
+    author = author.strip()
+      
+    description = '%s [%s]' % (wise_saw, author)
+    param = {'status': description}
+
+    client = oauth.TwitterClient(tokens.CONSUMER_KEY,
+                                 tokens.CONSUMER_SECRET, None)
+
+    if len(wise_saw) > 0 and len(author) > 0:
+      client.make_request(
+        UPDATE_URL,
+        token=tokens.ACCESS_TOKEN,
+        secret=tokens.ACCESS_TOKEN_SECRET,
+        additional_params=param,
+        protected=True,
+        method='POST'
+        )
+
+      self.response.out.write(wise_saw + ' : ' + author)
+
+    else:
+      self.response.out.write('wisesaw failed -> ' + wise_saw + ' : ' + author)
+
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler)],
                                        debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
+  util.run_wsgi_app(application)
+  
 
 
 if __name__ == '__main__':
